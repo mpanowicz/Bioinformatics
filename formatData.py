ae45caa446f6411a360f7899b17398916d3bffcc
mpanowicz <panowicz.michal@gmail.com>
2016-05-17 10:14:10 +0200

    Add logging to console/file

--- formatData.py
+++ formatData.py
@@ -3,20 +3,32 @@ import re
 from collections import Counter, OrderedDict
 import itertools
 
-toFile = False
+toFile = False			#Choose if create emptyMatrix file
+log = True				#Display additional info to console
+consoleLog = True		#Log all data froim console to file
+clearConsoleLog = True	#Create new console.log file
+
+if consoleLog :
+	if clearConsoleLog : open('console.log', 'w')
+	sys.stdout = open('console.log', 'a')
 
 def extractData():
+	if log : print('Read input file')
 	data = open(sys.argv[1], 'r').readlines()
 	out = open('out.seq', 'w')
 	text = ''.join(data)
+	regexText = 'ORIGIN(\s*([0-9][\satgc]*))*\/\/'
 	regex = re.compile('ORIGIN(\s*([0-9][\satgc]*))*\/\/', re.MULTILINE)
+	dnaText = '[atcg]+'
 	dna = re.compile('[atcg]+')
 	seqId = 1
-	
+	if log : print('Extract data based on regex ' + regexText)
 	for i in re.finditer(regex, text):
 		sequence = ''
+		if log : print('Extract data based on regex ' + dnaText)
 		for i in re.finditer(dna, i.group(0)):
 			sequence += i.group(0)
+		if log : print('If sequence length between 1000 and 1100 add it to file out.seq')
 		if len(sequence) >= 1000 and len(sequence) <= 1100:
 			out.write('seqId ' + str(seqId) + '. length ' + str(len(sequence)) + '\n')
 			out.write(sequence)
@@ -25,6 +37,7 @@ def extractData():
 	out.close()
 
 def cutSequence(seq):
+	if log : print('Cut given sequence into 10 letter length subsequences')
 	cut = []
 	for i in range(len(seq) - 9):
 		cut.append(seq[i:i+10])
@@ -35,9 +48,12 @@ def createEmptyMatrix():
 	if toFile :
 		out = open('emptyMatrix.out', 'w')
 	matrix = []
+	if log : print('Create all Variations with repetition for acgt and 10 letter length')
 	all = itertools.product('acgt', repeat=10)
 	id = 0
 	row = []
+	if log : print('Format (\'a\', \'c\', ...) objects into ac.. strings')
+	if log : print('Create matrix with elements {\'acgtactgaa\' : 0}')
 	for i in all:
 		if(id % 1024 == 0 and id != 0):
 			if toFile :
@@ -56,11 +72,11 @@ def createEmptyMatrix():
 	
 def countSubsequences():
 	seq = 'tttttttttttttttatttataacgcctattgtagattgataggctctaaatttacaaacctgaatcgtacagatttgtacgaccgtttgggaaaggtatgccattatcggatgtataaattaccaatgtatcatccttgaatcctgccttttctaattcttcaagaacaaggccaacgcctttatctaaacgagatattgttgtatattgggcagcaatatctcttcgagcagcttcggtattctgaacatagtatggtactttaacttgctcccattgataatatattggattccaatcgggaattgtacccattccaatatcaccattgccgaatttctcacaaaaattgccatactctggatgcgtatgcccacagcgatgcggatcgtgaaaggcaacatacagaaagaagggttgtgttttattttgtgaaagaaattcgcggacaagtagctttatataagtaatattgcgacctacttgaagaatagaattattttcttctgtatatgcaaaatcaaatggataaacattactgggacctacatgtttcttgccaataattcctgttcgtatattatttcttttcaatatttttggcaaacttttaatattatcaaaagaattaaaatgatgaactccttgatgaagaccatacatcccattttgatgacttggtaatccagttaatatcgtggagcggcttggtgaacagctactgactgatgtgtacgcattgttgaatagtagactttcctttgccaatttatccaaattaggagtttgacatatcttatttaagtacgatcgcatttcaaaacctgcatcatcagccagcaataggagcacattttttcgagatatatcggtttttccatttgtgcacaagacaatacacaaaagccataataatttattcaaacagctgttcaacgacatatcatacaattgtgaaacgtgcgtctgcgttagcggtatacgcgtgtaagcacgtgaccgtggctgcgttccgatatacactgca'
-	matrix = dict(Counter(cutSequence(seq)))
-	return matrix
+	cutted = cutSequence(seq)
+	if log : print('Count subsequences in given sequence')
+	subsequencesDict = dict(Counter(cutted))
+	orderedDict = OrderedDict(sorted(subsequencesDict.items()))
+	return orderedDict
 
-emptyMatrix = createEmptyMatrix()
-ordered = OrderedDict(sorted(countSubsequences().items()))
+countSubsequences()
 
-for i, j in ordered.items():
-	print(i, j)
